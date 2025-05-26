133c31463277e7f10324bf6c5ed55d08f90949f3
Tanel Poder <tanel@tanelpoder.com>
2025-05-26 03:36:32 -0400

    add stress test loop .sh scripts

---
+++ scripts/dog_loop.sh
@@ -0,0 +1,38 @@
+psql tpcc -f ./dog_loop.sql &
+psql tpcc -f ./dog_loop.sql &
+psql tpcc -f ./dog_loop.sql &
+psql tpcc -f ./dog_loop.sql &
+
+# uncomment more lines below if you want more concurrent workload
+
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+# psql tpcc -f ./dog_loop.sql &
+
+# kill all child processes on CTRL+C
+wait
